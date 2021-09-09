43671ab05ed585ca6d3d1de76ccbbdd6d0abcea1
AORLAN <asdrubalborrerorojas@gmail.com>
2021-09-09 16:52:53 -0400

    add treasury

---
+++ Dark_Treasury.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
+
+import "@openzeppelin/contracts/GSN/Context.sol"; 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+contract Dark_Treasury is Ownable {
+    using SafeERC20 for IERC20;
+
+    function withdrawTokens(address _token, address _to, uint256 _amount) external onlyOwner {
+        IERC20(_token).safeTransfer(_to, _amount);
+    }
+
+    function withdrawKcs (address payable _to, uint256 _amount) external onlyOwner {
+        _to.transfer(_amount);
+    }
+
+    receive () external payable {}
+}
