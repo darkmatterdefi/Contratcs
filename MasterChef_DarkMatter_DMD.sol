83f8a39eeaef3c7139f045a70068ecc5dabe3981
Aorlan <72566960+aobr95@users.noreply.github.com>
2021-09-28 09:09:40 -0400

    fix

--- MasterChef_DarkMatter_DMD.sol
+++ MasterChef_DarkMatter_DMD.sol
@@ -85,7 +85,7 @@ contract MasterChef_DarkMatter_DMD is Ownable, ReentrancyGuard {
     //a maximum of 2 per second is set.
     uint256 public constant maxDMDPerSecond = 2e18;
     // Timestamp startTime.
-    uint256 public immutable startTime;
+    uint256 public startTime;
 
     // events 
     event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
@@ -110,7 +110,7 @@ contract MasterChef_DarkMatter_DMD is Ownable, ReentrancyGuard {
         poolInfo.push(PoolInfo({
             lpToken: _DMD,
             allocPoint: 1000,
-            lastRewardTime: _startTime,
+            lastRewardTime: startTime,
             accDMDPerShare: 0,
             depositFeeBP: 0
         }));
